29fb8730d81e7608458bee2b95c35f5de99e518e
alan <alanarostegui@hotmail.com>
2022-04-29 00:41:54 -0500

    first commit

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+// Create next js pages/about.js:
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <main>
+        <h1>About</h1>
+        <p>
+          Get started by editing <code>pages/about.js</code>
+        </p>
+      </main>
+    </div>
+  );
+}
