dd15fa5ff00fa795721f2d4227ea4b602e813ea4
alan <alanarostegui@hotmail.com>
2022-05-06 00:05:47 -0500

    add navbar dynamically and add pricing page

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,12 +2,33 @@ import React from "react";
 import ActiveLink from "./ActiveLink";
 import styles from "./Navbar.module.css";
 
+const menuItems = [
+  {
+    text: "Home",
+    href: "/",
+  },
+  {
+    text: "About",
+    href: "/about",
+  },
+  {
+    text: "Contact",
+    href: "/contact",
+  },
+  {
+    text: "Pricing",
+    href: "/pricing",
+  },
+];
+
 const Navbar = () => {
   return (
     <nav className={styles["menu-container"]}>
-      <ActiveLink text="Home" href="/" />
-      <ActiveLink text="About" href="/about" />
-      <ActiveLink text="Contact" href="/contact" />
+      {menuItems.map((item) => (
+        <ActiveLink key={item.text} href={item.href} text={item.text}>
+          {item.text}
+        </ActiveLink>
+      ))}
     </nav>
   );
 };
---
+++ pages/pricing/index.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import MainLayout from "../../components/layouts/MainLayout";
+import DarkLayout from "../../components/layouts/DarkLayout";
+
+export default function Pricing() {
+  return (
+    <>
+      <h1 className="title">
+        Ir a <Link href="/">Home</Link>
+      </h1>
+      <p className="description">
+        Get started by editing <code>pages/Pricing.js</code>
+      </p>
+    </>
+  );
+}
+
+Pricing.getLayout = function getLayout(page) {
+  return (
+    <MainLayout>
+      <DarkLayout>{page}</DarkLayout>
+    </MainLayout>
+  );
+};
